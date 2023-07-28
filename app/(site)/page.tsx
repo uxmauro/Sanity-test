e908f1e7c8b920d22471178e861e63377fd45de9
Mauro <mauro.guerrero@glorifi.com>
2023-07-28 11:39:05 -0400

    fixed &apos

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
 
   return (
     <div> 
-    <h1 className="text-6xl font-bold">Hello I'm <span className=" bg-gradient-to-tr from-blue-600 to-green-300 bg-clip-text text-transparent">Mauro!</span></h1>
+    <h1 className="text-6xl font-bold">Hello I&apos;m <span className=" bg-gradient-to-tr from-blue-600 to-green-300 bg-clip-text text-transparent">Mauro!</span></h1>
     <p className="mt-3 text-xl text-gray-500"> Thanks you for visitng my site here are a few projects I have been working on:</p>
     
     <h2 className=" mt-24 font-bold text-gray-700 text-3xl">My Work</h2>
